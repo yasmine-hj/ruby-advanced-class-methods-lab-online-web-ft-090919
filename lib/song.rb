ad30cc1ab21be98610a62c719f49599dbdcd3c78
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-21 09:42:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -57,7 +57,7 @@ class Song
     song_name = parts[1].gsub(".mp3", "")
     
     song = self.create
-    sone.name = song_name
+    song.name = song_name
     song.artist_name = artist_name
     song
   end
